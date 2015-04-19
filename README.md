871b061337cc1728f4d3b38b4fe1598fa33a53c8
mscuaycong <mscuaycong@gmail.com>
2015-04-19 00:33:43 -0700

    Provided the README.md for users. Added the R script Added the meandat.txt output from the data transformation.

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
+
 # GetandCleanDataProject
 Course project for the Getting and Cleaning Data course
+
+##FILES INCLUDED
+run_analysis.R - R script to fulfill the requirements of the course data project in the "Getting and Cleaning Data" course
+ 
+meandat.txt - tidy dataset containing the mean values of the training and test data from the
+  "Human Activity Recognition Using Smartphones Dataset Version 1.0"
+
+
+##Data Input Source:
+[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+This dataset is distributed AS-IS and no responsibility implied or explicit can be addressed to the authors or their institutions for its use or misuse. Any commercial use is prohibited.
+Jorge L. Reyes-Ortiz, Alessandro Ghio, Luca Oneto, Davide Anguita. November 2012.
+##Run_analysis.R
+The script creates a tidy dataset using the files from the Human Ativity Recognition Using Smartphones Dataset
+Assumes that the data files are in the same directory as the directory from which this script is run.
+Also assumes that the files and subdirectories are organized the same way as the original, with its root in directory
+of the script.
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+
+##Source:
+Jorge L. Reyes-Ortiz(1,2), Davide Anguita(1), Alessandro Ghio(1), Luca Oneto(1) and Xavier Parra(2)
+1 - Smartlab - Non-Linear Complex Systems Laboratory
+DITEN - Università degli Studi di Genova, Genoa (I-16145), Italy. 
+2 - CETpD - Technical Research Centre for Dependency Care and Autonomous Living
+Universitat Politècnica de Catalunya (BarcelonaTech). Vilanova i la Geltrú (08800), Spain
+activityrecognition '@' smartlab.ws
+
+
+##Data Set Information:
+The experiments have been carried out with a group of 30 volunteers within an age bracket of 19-48 years. 
+Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING)
+wearing a smartphone (Samsung Galaxy S II) on the waist. Using its embedded accelerometer and gyroscope, 
+we captured 3-axial linear acceleration and 3-axial angular velocity at a constant rate of 50Hz. 
+The experiments have been video-recorded to label the data manually. The obtained dataset has been 
+randomly partitioned into two sets, where 70% of the volunteers was selected for generating the 
+training data and 30% the test data. 
+The sensor signals (accelerometer and gyroscope) were pre-processed by applying noise filters and then sampled in fixed-width sliding windows of 2.56 sec and 50% overlap (128 readings/window). The sensor acceleration signal, which has gravitational and body motion components, was separated using a Butterworth low-pass filter into body acceleration and gravity. The gravitational force is assumed to have only low frequency components, therefore a filter with 0.3 Hz cutoff frequency was used. From each window, a vector of features was obtained by calculating variables from the time and frequency domain.
+
+##output: meandata.txt - means of all of the variables grouped by Activity Name (g.actname)
+and Subject who did it (g.subjID)
+Variable (colname)	Description/Values
+g.actname		Any of six activities: WALKING,WALKING_UPSTAIRSWALKING_DOWNSTAIRS,SITTING,STANDING,LAYING
+g.subjID		The subject ID - identifies the volunteer who did the activity
+tBodyAcc-mean()-X	Variables that start with t - refer to time
+tBodyAcc-mean()-Y	Variables that start with f - refer to frequency
+tBodyAcc-mean()-Z	
+tGravityAcc-mean()-X	
+tGravityAcc-mean()-Y	
+tGravityAcc-mean()-Z	
+tBodyAccJerk-mean()-X	
+tBodyAccJerk-mean()-Y	
+...Etc...
---
+++ run_analysis.R
@@ -0,0 +1,131 @@
+
+#set working directory
+#../courseproject
+
+library(data.table)
+library(dplyr)
+library(plyr)
+list.files("./UCI HAR Dataset/train")
+
+traindir<-"./UCI HAR Dataset/train/"
+testdir<-"./UCI HAR Dataset/test/"
+
+##READ files common to TRAIN and TEST Groups
+#read feature names
+feat<-read.table("./UCI HAR Dataset/features.txt")
+feat<-data.table(feat)
+
+act_labels<-read.table("./UCI HAR Dataset/activity_labels.txt")
+act_labels<-data.table(act_labels)
+
+##TRAIN data begins here
+#Read the 'train/subject_train.txt'
+#Each row identifies the subject who performed the activity for each window sample. 
+#Its range is from 1 to 30. 
+
+t_subj<-read.table(paste0(traindir,"subject_train.txt"))
+t_subj<-data.table(t_subj)
+setnames(t_subj,"V1","subjID")
+
+#read the training set- the header is V1:V561 0 variables for the features
+t_X<-read.table(paste0(traindir,"X_train.txt"))
+#convert to data.table for speed and other functions
+t_X<-data.table(t_X)
+#read the training labels which contain the activity code
+t_Y<-read.table(paste0(traindir,"Y_train.txt"))
+t_Y<-data.table(t_Y)
+
+
+t_dat<-cbind(t_subj,t_Y)
+t_dat<-tbl_df(t_dat)
+
+rm(t_Y,t_subj) # clear vars - don't need these interim tables
+
+
+
+
+#map the training data set variable names to the feature name
+setnames(t_X,names(t_X),as.character(feat[,V2]))
+
+#merge the subject data with the feature results
+
+traindata<-cbind(t_dat,t_X)
+rm(t_dat,t_X)
+
+#get the label values for the activity
+traindata <- join(traindata, act_labels, by = "V1")
+#Rename the var columns 
+setnames(traindata,c("V1","V2"),c("actcode","actname"))
+#move the last column which is now the actname close to actcode
+traindata<-select(traindata,subjID,actcode,actname,3:563)
+traindata<-mutate(traindata,settype="train")
+##END retrieving Train data
+
+
+## DO the same transformations for TEST data
+##TEST data begins here
+
+testdir<-"./UCI HAR Dataset/test/"
+#Read the 'test/subject_test.txt'
+#Each row identifies the subject who performed the activity for each window sample. 
+#Its range is from 1 to 30. 
+
+test_subj<-read.table(paste0(testdir,"subject_test.txt"))
+test_subj<-data.table(test_subj)
+setnames(test_subj,"V1","subjID")
+
+#read the TEST set- the header is V1:V561 0 variables for the features
+test_X<-read.table(paste0(testdir,"X_test.txt"))
+#convert to data.table for speed and other functions
+test_X<-data.table(test_X)
+#read the training labels which contain the activity code
+test_Y<-read.table(paste0(testdir,"Y_test.txt"))
+test_Y<-data.table(test_Y)
+
+
+test_dat<-cbind(test_subj,test_Y)
+test_dat<-tbl_df(test_dat)
+
+rm(test_Y,test_subj) # clear vars - don't need these interim tables
+
+
+#map the training data set variable names to the feature name
+setnames(test_X,names(test_X),as.character(feat[,V2]))
+
+#Merge the subject data with the feature results
+
+testdata<-cbind(test_dat,test_X)
+rm(test_dat,test_X)
+
+#get the label values for the activity
+testdata <- join(testdata, act_labels, by = "V1")
+#Rename the var columns 
+setnames(testdata,c("V1","V2"),c("actcode","actname"))
+#move the last column which is now the actname close to actcode
+testdata<-select(testdata,subjID,actcode,actname,3:563)
+testdata<-mutate(testdata,settype="test")
+## END retrieving TEST data
+
+## Combine Training and Test Data
+alldata<-rbind(traindata,testdata)
+# this is now a Tidy data set
+rm(testdata,traindata)#remove this - no longer necessary
+
+#select only the mean and std columns
+alldata_1<-select(alldata,settype,subjID,actcode,actname,
+                    contains("mean"),contains("std"))
+
+ 
+##Create another tidy dataset with only the means of the variables
+## grouped by Activity and by Subject
+by_act_sub<- select(alldata_1,actname,subjID,5:90)
+meandat<-aggregate(by_act_sub,
+               by=list(g.actname=by_act_sub$actname,g.subjID=by_act_sub$subjID),
+               mean)
+meandat<-meandat%>%
+    arrange(g.actname,g.subjID)%>%
+    select(-actname,-subjID)
+
+#output Meandat which contains the means to a text file.
+write.table(meandat,file="./meandat.txt",row.names=FALSE)
+
